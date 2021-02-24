e2364f2161a7fee2f79a26b686c3f5a0d60e54e2
paul-E404 <paul_engerling@web.de>
2021-02-24 13:30:02 +0100

    Added animated winner lines

--- script.js
+++ script.js
@@ -36,30 +36,45 @@ function showActivePlayer() {
 function checkForWinner() {
 
     let winner;
-
+    //First Row
     if(field[0] == field[1] && field[1] == field[2] && field[0]) {
         winner = field[0];
+        document.getElementById('first-row').style.transform = 'scaleX(1.0)';
     }
+    //Second Row
     if(field[3] == field[4] && field[4] == field[5] && field[3]) {
         winner = field[3];
+        document.getElementById('second-row').style.transform = 'scaleX(1.0)';
     }
+    //Third Row
     if(field[6] == field[7] && field[7] == field[8] && field[6]) {
         winner = field[6];
+        document.getElementById('third-row').style.transform = 'scaleX(1.0)';
     }
+    //First Column
     if(field[0] == field[3] && field[3] == field[6] && field[0]) {
         winner = field[0];
+        document.getElementById('first-column').style.transform = 'scaleY(1.0)';
     }
+    //Second Column
     if(field[1] == field[4] && field[4] == field[7] && field[1]) {
         winner = field[1];
+        document.getElementById('second-column').style.transform = 'scaleY(1.0)';
     }
+    //Third Column
     if(field[2] == field[5] && field[5] == field[8] && field[2]) {
         winner = field[2];
+        document.getElementById('third-column').style.transform = 'scaleY(1.0)';
     }
+    //Diagonal From Top Left to Bottom Right
     if(field[0] == field[4] && field[4] == field[8] && field[0]) {
         winner = field[0];
+        document.getElementById('first-diagonal').style.transform = 'scale(1.0) rotate(45deg)';
     }
+    //Diagonal From Bottom Left to Top Right
     if(field[2] == field[4] && field[4] == field[6] && field[2]) {
         winner = field[2];
+        document.getElementById('second-diagonal').style.transform = 'scale(1.0) rotate(-45deg)';
     }
 
     /* !! Convert to boolen */
